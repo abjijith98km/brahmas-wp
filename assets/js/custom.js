79c803bdd6c38269a9f0b8e26eac4b5053a275dc
abhijith <abhijith@pixelflames.com>
2022-11-28 18:56:31 +0530

    asd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,17 @@
-$(window).scroll(function(){
-  if(window.scrollY > 240){
-    $('body').addClass('sticky_header')
-  }else{
-    $('body').removeClass('sticky_header')
+$(window).scroll(function () {
+  if (window.scrollY > 240) {
+    $("body").addClass("sticky_header");
+  } else {
+    $("body").removeClass("sticky_header");
   }
-})
+});
+var thumbs = document.querySelectorAll(
+  ".custom_course_details_page_block .coursr_detail_block .woocommerce-product-gallery.woocommerce-product-gallery--with-images.woocommerce-product-gallery--columns-4.images .woocommerce-product-gallery__image:not(:first-of-type)"
+);
+thumbs.forEach((item) =>
+  item.addEventListener("click",function (e) {
+    e.preventDefault();
+    $(".custom_course_details_page_block .coursr_detail_block .woocommerce-product-gallery.woocommerce-product-gallery--with-images.woocommerce-product-gallery--columns-4.images .woocommerce-product-gallery__image:first-of-type a img").attr("src",`${$(item).find("img").attr("src")}`);
+    $(".custom_course_details_page_block .coursr_detail_block .woocommerce-product-gallery.woocommerce-product-gallery--with-images.woocommerce-product-gallery--columns-4.images .woocommerce-product-gallery__image:first-of-type a").attr("href",`${$(item).find("img").attr("src")}`);
+  })
+);
